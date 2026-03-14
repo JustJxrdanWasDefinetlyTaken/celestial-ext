e276fb053d73d06ccd7df63e4f7a622aef78727b
JRDN <108704541+JustJxrdanWasDefinetlyTaken@users.noreply.github.com>
2026-03-14 14:39:31 -0700

    Update yt-adblocker.js

--- ext/yt-adblocker.js
+++ ext/yt-adblocker.js
@@ -1,10 +1,1 @@
-javascript:(function(){
-  if(window.zkAdSkipper) return;
-  window.zkAdSkipper = setInterval(()=>{
-    const v = document.querySelector('video');
-    const ad = document.querySelector('.ad-showing, .ytp-ad-module-visible');
-    const skip = document.querySelector('.ytp-ad-skip-button-modern, .ytp-ad-skip-button');
-    if(ad && v) v.currentTime = v.duration;
-    if(skip) skip.click();
-  }, 500);
-})();
+
